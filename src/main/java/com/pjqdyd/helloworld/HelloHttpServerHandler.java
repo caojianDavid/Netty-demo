a2d4231e5cac843b40e084742e819b42c5a2379a
pjqdyd <1909025079@qq.com>
2019-09-23 12:44:15 +0800

    --init--

--- src/main/java/com/pjqdyd/helloworld/HelloHttpServerHandler.java
+++ src/main/java/com/pjqdyd/helloworld/HelloHttpServerHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**   
  * @Description:  [自定义处理器]
  * @Author:       pjqdyd
@@ -18,18 +20,25 @@ public class HelloHttpServerHandler extends SimpleChannelInboundHandler<HttpObje
     //接收读取客户端发送的请求, 并响应
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-        //if(msg instanceof HttpRequest){//包裹下面代码}
+        if(msg instanceof HttpRequest){
+            HttpRequest httpRequest = (HttpRequest) msg;
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())){
+                System.out.println("网页请求图标");
+                return;
+            }
 
-        //定义响应的内容
-        ByteBuf content = Unpooled.copiedBuffer("Hello World!", CharsetUtil.UTF_8);
-        //响应体
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
-                HttpResponseStatus.OK, content);
-        //设置响应头
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            //定义响应的内容
+            ByteBuf content = Unpooled.copiedBuffer("Hello World!", CharsetUtil.UTF_8);
+            //响应体
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.OK, content);
+            //设置响应头
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
 
-        //发送响应
-        ctx.writeAndFlush(response);
+            //发送响应
+            ctx.writeAndFlush(response);
+        }
     }
 }
